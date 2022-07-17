7d24d9aa9e2132405a98348e89657d4a40e82b2c
Safi <safikurrahman7514@gmail.com>
2022-07-17 08:03:34 +0600

    add navigation menu dropdown and active link

---
+++ src/components/Navbar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = ({ children }) => {
+    return (
+        <>
+            <div class="drawer drawer-end ">
+                <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+                <div class="drawer-content flex flex-col ">
+                    <div class="w-full navbar bg-base-200 lg:px-20">
+                        <div class="flex-1 px-2 mx-2">Clean-co.</div>
+                        <div class="flex-none lg:hidden">
+                            <label for="my-drawer-3" class="btn btn-square btn-ghost">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                            </label>
+                        </div>
+                        <div class="flex-none hidden lg:block">
+                            <ul class="menu menu-horizontal">
+                                <li><NavLink className='rounded-lg mx-1' to='/'>Home</NavLink></li>
+                                <li><NavLink className='rounded-lg mx-1' to='/about'>About</NavLink></li>
+                                <li><NavLink className='rounded-lg mx-1' to='/services'>Services</NavLink></li>
+                                <li><NavLink className='rounded-lg mx-1' to='/contact'>Contact</NavLink></li>
+                                <li><NavLink className='rounded-lg mx-1' to='/login'>Login</NavLink></li>
+                                <div class="flex justify-end flex-1 ">
+                                    <div class="flex items-stretch">
+                                        <div class="dropdown dropdown-end">
+                                            <label tabindex="0" class="btn btn-outline btn-primary rounded-btn">BOOK NOW</label>
+                                            <ul tabindex="0" class="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                                                <li><a>Item 1</a></li>
+                                                <li><a>Item 2</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </ul>
+                        </div>
+                    </div>
+                    {children}
+                </div>
+                <div class="drawer-side">
+                    <label for="my-drawer-3" class="drawer-overlay"></label>
+                    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
+                        <li><NavLink className='rounded-lg' to='/'>Home</NavLink></li>
+                        <li><NavLink className='rounded-lg' to='/about'>About</NavLink></li>
+                        <li><NavLink className='rounded-lg' to='/services'>Services</NavLink></li>
+                        <li><NavLink className='rounded-lg' to='/contact'>Contact</NavLink></li>
+                        <li><NavLink className='rounded-lg' to='/login'>Login</NavLink></li>
+                    </ul>
+
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Navbar;
